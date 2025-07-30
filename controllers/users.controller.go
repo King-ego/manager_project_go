fb20fc19d4c283f91b6f7de2ce508676f32605a3
Diego <diegosantos4957@gmail.com>
2025-07-30 08:35:08 -0300

    feat: validate sender field request

--- controllers/users.controller.go
+++ controllers/users.controller.go
@@ -1,6 +1,10 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 type UserController struct {
 }
@@ -16,4 +20,21 @@ func (uc *UserController) GetUsers(ctx *gin.Context) {
 		"data":    users,
 	})
 
+}
+
+type CreateUserRequest struct {
+	Name  string `json:"name" binding:"required"`
+	Email string `json:"email" binding:"required,email"`
+}
+
+func (uc *UserController) CreateUser(ctx *gin.Context) {
+	var newUser CreateUserRequest
+	if err := ctx.ShouldBindJSON(&newUser); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusCreated, gin.H{
+		"message": "User created successfully",
+		"data":    newUser,
+	})
 }
