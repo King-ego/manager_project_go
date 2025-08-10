72de593ba0ece8005692f9c2b2e5032255ebb721
Diego <diegosantos4957@gmail.com>
2025-08-10 11:46:06 -0300

    feat: remove close main at execute serve.RUN

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,15 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"log"
 	"manager_project/router"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,5 +25,29 @@ func main() {
 		})
 	})
 
-	server.Run(":8989")
+	srv := &http.Server{
+		Addr:    ":8989",
+		Handler: server,
+	}
+
+	go func() {
+		log.Printf("server listen at %s", srv.Addr)
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	} else {
+		log.Println("Server exiting gracefully")
+	}
 }
