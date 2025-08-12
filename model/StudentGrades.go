b4ad1d9cda359369edbf5fbf4b9f29d825d13b3a
Diego <diegosantos4957@gmail.com>
2025-08-11 22:20:05 -0300

    feat: remove curse id

--- model/StudentGrades.go
+++ model/StudentGrades.go
@@ -2,7 +2,6 @@ package model
 
 type StudentGrades struct {
 	StudentID string `json:"student_id"`
-	CourseID  string `json:"course_id"`
 	Year      int    `json:"year"`
 	Semester  int    `json:"semester"`
 }
