947fd6858cb9a6e62943a9cf9c3b552c00c2fd54
Diego <diegosantos4957@gmail.com>
2025-08-12 23:12:12 -0300

    feat: update fields in classes entity

--- models/Classes.go
+++ models/Classes.go
@@ -1,7 +1,17 @@
 package models
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type Classes struct {
-	StudentID string `json:"student_id"`
-	Year      int    `json:"year"`
-	Semester  int    `json:"semester"`
+	gorm.Model
+	ID        string    `gorm:"primaryKey;default:uuid_generate_v4();type:uuid" json:"id"`
+	TeacherID string    `gorm:"type:uuid" json:"teacher_id"`
+	Year      int       `gorm:"type:int" json:"year"`
+	Semester  int       `gorm:"type:int" json:"semester"`
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
 }
