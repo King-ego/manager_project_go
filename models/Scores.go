4487618a8eb86c96e66c413851d745e9c0b7273a
Diego <diegosantos4957@gmail.com>
2025-08-13 22:23:20 -0300

    feat: update score data entity

--- models/Scores.go
+++ models/Scores.go
@@ -1,7 +1,16 @@
 package models
 
+import (
+	"gorm.io/gorm"
+)
+
 type Score struct {
-	StudentID string  `json:"student_id"`
-	Subject   string  `json:"subject"`
-	Score     float64 `json:"score"`
+	gorm.Model
+	ID             string `gorm:"primaryKey; unique; type: uuid; default: uuid_generate_v4()" json:"id"`
+	EnrollmentID   string `gorm:"not null;type:uuid" json:"enrollment_id"`
+	ScoreValue     int    `gorm:"not null;type:int" json:"score_value"`
+	AssessmentType string `gorm:"not null;type:text" json:"assessment_type"`
+	AssessmentDate string `gorm:"not null;type:text" json:"assessment_date"`
+	Weight         int    `gorm:"not null;type:int" json:"weight"`
+	Comments       string `gorm:"type:text" json:"comments"`
 }
