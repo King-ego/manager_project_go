4609d2a017c823cd29b257637e2300bd7cbf1dc5
Diego <diegosantos4957@gmail.com>
2025-08-13 22:25:49 -0300

    feat: update teacher.go

--- models/Teachers.go
+++ models/Teachers.go
@@ -1,1 +1,18 @@
 package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Teacher struct {
+	gorm.Model
+	ID             string    `gorm:"primaryKey;unique;type:uuid;default:uuid_generate_v4()" json:"id"`
+	Name           string    `gorm:"size:100;not null" json:"name"`
+	Email          string    `gorm:"size:100;not null" json:"email"`
+	Specialization string    `gorm:"size:100;not null" json:"specialization"`
+	HireDate       string    `gorm:"type:text" json:"hire_date"`
+	CreatedAt      time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt      time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+}
