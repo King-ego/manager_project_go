f1bf52b56418d6ecf2c0dfb6e4b813be45112c3b
Diego <diegosantos4957@gmail.com>
2025-08-15 10:40:06 -0300

    feat: rename user dto to student dto in router

--- dto/create_students_dto.go
+++ dto/create_students_dto.go
@@ -1,6 +1,6 @@
 package dto
 
-type CreateUserDTO struct {
+type CreateStudentsDTO struct {
 	Name      string `json:"name" binding:"required"`
 	Email     string `json:"email" binding:"required,email"`
 	Password  string `json:"password" binding:"required"`
