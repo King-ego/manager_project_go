57175802ee660ae747afd2b713530a7335955964
Diego <diegosantos4957@gmail.com>
2025-08-16 17:58:53 -0300

    feat: method create teacher

--- repositories/teacher.repository.go
+++ repositories/teacher.repository.go
@@ -1,1 +1,19 @@
 package repositories
+
+import "gorm.io/gorm"
+
+type TeacherRepository interface {
+	Save() error
+}
+
+type teacherRepository struct {
+	db *gorm.DB
+}
+
+func NewStudentRepository(db *gorm.DB) TeacherRepository {
+	return &teacherRepository{db: db}
+}
+
+func (r *teacherRepository) Save() error {
+	return r.db.Create(&struct{}{}).Error
+}
