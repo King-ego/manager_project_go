9bc2407dbc6faee9f50618aa9e85f5f73ec22c02
Diego <diegosantos4957@gmail.com>
2025-08-16 18:07:42 -0300

    feat: update controller teacher

--- controllers/teacher.controller.go
+++ controllers/teacher.controller.go
@@ -1,1 +1,26 @@
 package controllers
+
+import (
+	"manager_project/dto"
+
+	"github.com/gin-gonic/gin"
+)
+
+type TeacherController struct{}
+
+func NewTeacherController() *TeacherController {
+	return &TeacherController{}
+}
+
+func (t *TeacherController) CreateTeacher(c *gin.Context) {
+	var createTeacherDTO dto.CreateTeacherDTO
+
+	if err := c.ShouldBindJSON(&createTeacherDTO); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	text := "Teacher created successfully"
+
+	c.JSON(201, gin.H{"data": text})
+}
