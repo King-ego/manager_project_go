8fb84a61d8cf7f8f04531a77f90a49484066bce1
Diego <diegosantos4957@gmail.com>
2025-08-16 17:52:48 -0300

    feat: create setup router

--- routers/teachers.router.go
+++ routers/teachers.router.go
@@ -1,1 +1,32 @@
 package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type TeacherRouter struct {
+	server *gin.Engine
+	db     *gorm.DB
+}
+
+func NewTeacherRouter(server *gin.Engine, db *gorm.DB) *TeacherRouter {
+	return &TeacherRouter{
+		server: server,
+		db:     db,
+	}
+
+}
+
+func (r *TeacherRouter) setupTeacherRouters() {
+
+}
+
+func (r *TeacherRouter) TeacherRouters() {
+	r.setupTeacherRouters()
+}
+
+func SetupTeacherRoutes(server *gin.Engine, db *gorm.DB) {
+	router := NewTeacherRouter(server, db)
+	router.TeacherRouters()
+}
