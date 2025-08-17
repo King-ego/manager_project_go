42773a52c45b9fe6895c2703502d8acb948ef2eb
Diego <diegosantos4957@gmail.com>
2025-08-17 11:58:16 -0300

    feat: type text to timestamp teacher entity

--- models/Teachers.go
+++ models/Teachers.go
@@ -12,7 +12,7 @@ type Teacher struct {
 	Name           string    `gorm:"size:100;not null" json:"name"`
 	Email          string    `gorm:"size:100;not null" json:"email"`
 	Specialization string    `gorm:"size:100;not null" json:"specialization"`
-	HireDate       string    `gorm:"type:text" json:"hire_date"`
+	HireDate       time.Time `gorm:"type:timestamp" json:"hire_date"`
 	CreatedAt      time.Time `gorm:"autoCreateTime" json:"created_at"`
 	UpdatedAt      time.Time `gorm:"autoUpdateTime" json:"updated_at"`
 }
