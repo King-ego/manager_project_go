bf9beee83eb422ea7d403b3e6d464f1db4bfb8c3
Diego <diegosantos4957@gmail.com>
2025-08-19 20:42:47 -0300

    feat: add GetByID method to StudentsRepository

--- repositories/students.repository.go
+++ repositories/students.repository.go
@@ -21,3 +21,12 @@ func NewStudentsRepository(db *gorm.DB) StudentsRepository {
 func (r *studentsRepository) Save(student *models.Students) error {
 	return r.db.Create(student).Error
 }
+
+func (r *studentsRepository) GetByID(studentId string) (models.Students, error) {
+	var student models.Students
+	err := r.db.Where("id = ?", studentId).First(&student).Error
+	if err != nil {
+		return models.Students{}, err
+	}
+	return student, nil
+}
