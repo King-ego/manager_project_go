fa28b8eb7c815eb1594599090d669e9015d194fa
Diego <diegosantos4957@gmail.com>
2025-08-21 18:06:40 -0300

    feat: update NewScoreUseCase to accept ScoreRepository and modify CreateScore method

--- usecases/score.usecase.go
+++ usecases/score.usecase.go
@@ -1,12 +1,24 @@
 package usecases
 
+import (
+	"manager_project/models"
+	"manager_project/repositories"
+)
+
 type ScoreUseCase struct {
+	repository repositories.ScoreRepository
 }
 
-func NewScoreUseCase() *ScoreUseCase {
-	return &ScoreUseCase{}
+func NewScoreUseCase(repository repositories.ScoreRepository) *ScoreUseCase {
+	return &ScoreUseCase{
+		repository: repository,
+	}
 }
 
 func (s *ScoreUseCase) CreateScore() (string, error) {
+	err := s.repository.CreateScore(&models.Score{})
+	if err != nil {
+		return "", err
+	}
 	return "", nil
 }
