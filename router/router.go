a952ae68735cc09075900238ca9aaddada142fbc
Diego <diegosantos4957@gmail.com>
2025-08-21 17:23:04 -0300

    feat: add SetupScoreRoutes to initialize score-related routes in router

--- router/router.go
+++ router/router.go
@@ -22,6 +22,7 @@ func NewRouter(server *gin.Engine, db *gorm.DB) *SetupRouter {
 func (r *SetupRouter) setupRouters() {
 	routers.SetupStudentsRoutes(r.server, r.db)
 	routers.SetupTeacherRoutes(r.server, r.db)
+	routers.SetupScoreRoutes(r.server, r.db)
 }
 
 func (r *SetupRouter) Routers() {
