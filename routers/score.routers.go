9fa567e20000150f2bf5aba93a6509414c47e58e
Diego <diegosantos4957@gmail.com>
2025-08-21 17:23:15 -0300

    feat: add ScoreRouters to initialize score-related routes in router

---
+++ routers/score.routers.go
@@ -0,0 +1,39 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type ScoreRouters struct {
+	db     *gorm.DB
+	server *gin.Engine
+}
+
+func NewScoreRouters(server *gin.Engine, db *gorm.DB) *ScoreRouters {
+	return &ScoreRouters{
+		db:     db,
+		server: server,
+	}
+}
+
+func (r *ScoreRouters) setupScoreRouters() {
+	// scoreRepository := repositories.NewScoreRepository(r.db)
+	// scoreUseCase := usecases.NewScoreUseCase(scoreRepository)
+	// scoreController := controllers.NewScoreController(scoreUseCase)
+
+	scores := r.server.Group("/scores")
+	{
+		// scores.POST("/", scoreController.CreateScore)
+		scores.GET("/:scoreId")
+	}
+}
+
+func (r *ScoreRouters) ScoreRouters() {
+	r.setupScoreRouters()
+}
+
+func SetupScoreRoutes(server *gin.Engine, db *gorm.DB) {
+	router := NewScoreRouters(server, db)
+	router.ScoreRouters()
+}
