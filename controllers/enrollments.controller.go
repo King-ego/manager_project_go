ead9e4bd5e92397b0dea1c6337acf49ddb3cc1f5
Diego <diegosantos4957@gmail.com>
2025-08-23 23:08:03 -0300

    feat: implement EnrollmentsController with CreateEnrollment method

--- controllers/enrollments.controller.go
+++ controllers/enrollments.controller.go
@@ -1,1 +1,11 @@
 package controllers
+
+type EnrollmentsController struct{}
+
+func NewEnrollmentsController() *EnrollmentsController {
+	return &EnrollmentsController{}
+}
+
+func (ec *EnrollmentsController) CreateEnrollment() string {
+	return "Enrollment created"
+}
