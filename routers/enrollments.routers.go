8171eb32dcd1686356fc1a1e45c6a7c45d81df8e
Diego <diegosantos4957@gmail.com>
2025-08-25 21:22:12 -0300

    feat: update EnrollmentsRouters to accept server and db parameters

--- routers/enrollments.routers.go
+++ routers/enrollments.routers.go
@@ -1,9 +1,20 @@
 package routers
 
-type EnrollmentsRouters struct{}
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
 
-func NewEnrollmentsRoutes() *EnrollmentsRouters {
-	return &EnrollmentsRouters{}
+type EnrollmentsRouters struct {
+	server *gin.Engine
+	db     *gorm.DB
+}
+
+func NewEnrollmentsRoutes(server *gin.Engine, db *gorm.DB) *EnrollmentsRouters {
+	return &EnrollmentsRouters{
+		server: server,
+		db:     db,
+	}
 }
 
 func (er *EnrollmentsRouters) setupEnrollmentsRoutes() string {
@@ -14,7 +25,7 @@ func (er *EnrollmentsRouters) EnrollmentsRoutes() {
 	er.setupEnrollmentsRoutes()
 }
 
-func SetupEnrollmentsRoutes() {
-	router := NewEnrollmentsRoutes()
+func SetupEnrollmentsRoutes(server *gin.Engine, db *gorm.DB) {
+	router := NewEnrollmentsRoutes(server, db)
 	router.EnrollmentsRoutes()
 }
