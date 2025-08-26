2d91b728babe003dee7aa944bd47748b454c941e
Diego <diegosantos4957@gmail.com>
2025-08-25 22:42:29 -0300

    feat: add ClassesRouter for managing class routes

--- routers/classes.routers.go
+++ routers/classes.routers.go
@@ -1,1 +1,34 @@
 package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type ClassesRouter struct {
+	server *gin.Engine
+	db     *gorm.DB
+}
+
+func NewClassesRouter(server *gin.Engine, db *gorm.DB) *ClassesRouter {
+	return &ClassesRouter{
+		server: server,
+		db:     db,
+	}
+}
+
+func (pr *ClassesRouter) setupClassesRouter() {
+	classes := pr.server.Group("/classes")
+	{
+		classes.GET("/")
+	}
+}
+
+func (pr *ClassesRouter) ClassesRouter() {
+	pr.setupClassesRouter()
+}
+
+func SetupClassesRoutes(server *gin.Engine, db *gorm.DB) {
+	router := NewClassesRouter(server, db)
+	router.ClassesRouter()
+}
