cc7fdd4fd0371870724c0d1350bc84b4b56d1a0e
Diego <diegosantos4957@gmail.com>
2025-08-26 08:47:46 -0300

    feat: add ToModel method to CreateClassesDTO for model conversion

--- dto/create_classes.dto.go
+++ dto/create_classes.dto.go
@@ -1,9 +1,21 @@
 package dto
 
+import "manager_project/models"
+
 type CreateClassesDTO struct {
 	ClassName    string `json:"class_name"`
 	TeacherID    string `json:"teacher_id"`
 	AcademicYear int    `json:"academic_year"`
 	Semester     int    `json:"semester"`
 	RoomNumber   int    `json:"room_number"`
+}
+
+func (dto *CreateClassesDTO) ToModel() *models.Classes {
+	return &models.Classes{
+		ClassName:    dto.ClassName,
+		TeacherID:    dto.TeacherID,
+		AcademicYear: dto.AcademicYear,
+		Semester:     dto.Semester,
+		RoomNumber:   dto.RoomNumber,
+	}
 }
