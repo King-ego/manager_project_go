e3dda848884b3c24c3d7fff96bc22e0faa0ef466
Diego <diegosantos4957@gmail.com>
2025-08-27 22:09:41 -0300

    feat: add CreateScoreDTO for score management

--- dto/create_score.dto.go
+++ dto/create_score.dto.go
@@ -1,1 +1,10 @@
 package dto
+
+type CreateScoreDTO struct {
+	Comment        string  `json:"comment"`
+	ScoreValue     float64 `json:"score_value" binding:"required"`
+	Weight         float64 `json:"weight" binding:"required"`
+	AssessmentDate string  `json:"assessment_date"`
+	AssessmentType string  `json:"assessment_type" binding:"required"`
+	EnrollmentID   string  `json:"enrollment_id" binding:"required"`
+}
