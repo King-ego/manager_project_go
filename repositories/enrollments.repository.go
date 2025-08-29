ef498170b3c7739a01324099a6e33ffc15956e5a
Diego <diegosantos4957@gmail.com>
2025-08-28 23:23:27 -0300

    feat: implement EnrollmentsRepository with CreateEnrollment method

--- repositories/enrollments.repository.go
+++ repositories/enrollments.repository.go
@@ -1,4 +1,26 @@
 package repositories
 
+import (
+	"manager_project/models"
+
+	"gorm.io/gorm"
+)
+
 type EnrollmentsRepository interface {
+	CreateEnrollment(enrollment models.Enrollment) error
+}
+
+type enrollmentsRepository struct {
+	db *gorm.DB
+}
+
+func NewEnrollmentsRepository(db *gorm.DB) EnrollmentsRepository {
+	return &enrollmentsRepository{db: db}
+}
+
+func (r *enrollmentsRepository) CreateEnrollment(enrollment models.Enrollment) error {
+	if err := r.db.Create(&enrollment).Error; err != nil {
+		return err
+	}
+	return nil
 }
