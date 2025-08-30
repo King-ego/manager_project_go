cf771419f1b4344c0201b04113c6d234e916ac48
Diego <diegosantos4957@gmail.com>
2025-08-30 00:08:48 -0300

    feat: add ToEnrollmentModel method to CreateEnrollmentsDTO for model conversion

--- dto/create_enrollments.dto.go
+++ dto/create_enrollments.dto.go
@@ -1,7 +1,17 @@
 package dto
 
+import "manager_project/models"
+
+func (dto *CreateEnrollmentsDTO) ToEnrollmentModel() *models.Enrollment {
+	return &models.Enrollment{
+		StudentID: dto.StudentID,
+		ClassID:   dto.ClassID,
+		Status:    dto.Status,
+	}
+}
+
 type CreateEnrollmentsDTO struct {
 	StudentID string `json:"student_id" binding:"required"`
 	ClassID   string `json:"class_id" binding:"required"`
-	Status    *bool  `json:"status"`
+	Status    bool   `json:"status"`
 }
