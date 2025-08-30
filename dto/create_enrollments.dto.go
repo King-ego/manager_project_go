2201e6ea9f5862d8774b1f65d1e1158ef5a9ba03
Diego <diegosantos4957@gmail.com>
2025-08-29 23:22:48 -0300

    feat: add CreateEnrollmentsDTO struct for enrollment data transfer

--- dto/create_enrollments.dto.go
+++ dto/create_enrollments.dto.go
@@ -1,1 +1,7 @@
 package dto
+
+type CreateEnrollmentsDTO struct {
+	StudentID string `json:"student_id" binding:"required"`
+	ClassID   string `json:"class_id" binding:"required"`
+	Status    *bool  `json:"status"`
+}
