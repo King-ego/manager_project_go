2a50b9499fd6cf974ef059c0e2ae7295417acbff
Diego <diegosantos4957@gmail.com>
2025-08-30 00:09:31 -0300

    feat: implement CreateEnrollment method in EnrollmentsController with error handling

--- controllers/enrollments.controller.go
+++ controllers/enrollments.controller.go
@@ -1,11 +1,40 @@
 package controllers
 
-type EnrollmentsController struct{}
+import (
+	"manager_project/dto"
+	"manager_project/usecases"
 
-func NewEnrollmentsController() *EnrollmentsController {
-	return &EnrollmentsController{}
+	"github.com/gin-gonic/gin"
+)
+
+type EnrollmentsController struct {
+	enrollmentsUseCase *usecases.EnrollmentsUseCase
 }
 
-func (ec *EnrollmentsController) CreateEnrollment() string {
-	return "Enrollment created"
+func NewEnrollmentsController(useCase *usecases.EnrollmentsUseCase) *EnrollmentsController {
+	return &EnrollmentsController{
+		enrollmentsUseCase: useCase,
+	}
+}
+
+func (ec *EnrollmentsController) CreateEnrollment(c *gin.Context) {
+	enrollment := dto.CreateEnrollmentsDTO{}
+
+	if err := c.ShouldBindJSON(&enrollment); err != nil {
+		c.JSON(400, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	err := ec.enrollmentsUseCase.CreateEnrollment(enrollment)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": err.Error(),
+		})
+	}
+	c.JSON(201, gin.H{
+		"data": "Enrollment created successfully",
+	})
+
 }
