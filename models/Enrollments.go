071150f36bb487b790f73b5830b6a9c255659491
Diego <diegosantos4957@gmail.com>
2025-08-30 00:09:41 -0300

    feat: change Status field type from string to boolean in Enrollments model

--- models/Enrollments.go
+++ models/Enrollments.go
@@ -7,5 +7,5 @@ type Enrollment struct {
 	ID        string `gorm:"primaryKey; unique; type:uuid; default:uuid_generate_v4()" json:"id"`
 	StudentID string `gorm:"not null; type:uuid;" json:"student_id"`
 	ClassID   string `gorm:"not null; type:uuid;" json:"class_id"`
-	Status    string `gorm:"not null; type:boolean; default: true" json:"status"`
+	Status    bool   `gorm:"not null; type:boolean; default: true" json:"status"`
 }
