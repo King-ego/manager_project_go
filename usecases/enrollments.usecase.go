0ee4b6fe93728025c8206971cf4f4f689976f8c7
Diego <diegosantos4957@gmail.com>
2025-08-29 23:28:55 -0300

    feat: implement EnrollmentsUseCase for managing enrollment operations

--- usecases/enrollments.usecase.go
+++ usecases/enrollments.usecase.go
@@ -1,1 +1,24 @@
 package usecases
+
+import (
+	"manager_project/dto"
+	"manager_project/repositories"
+)
+
+type EnrollmentsUseCase struct {
+	repository repositories.EnrollmentsRepository
+}
+
+func NewEnrollmentsUseCase(repository repositories.EnrollmentsRepository) *EnrollmentsUseCase {
+	return &EnrollmentsUseCase{
+		repository: repository,
+	}
+}
+
+func (e *EnrollmentsUseCase) CreateEnrollment(enrollment dto.CreateEnrollmentsDTO) error {
+	err := e.repository.CreateEnrollment(enrollment)
+	if err != nil {
+		return nil
+	}
+	return err
+}
