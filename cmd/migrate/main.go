1a2af294b10b8e424b45ff330653fde49c3e0e1e
Diego <diegosantos4957@gmail.com>
2025-09-05 18:53:29 -0300

    chore: update main.go for improved variable naming and clarity

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"log"
-	"manager_project/config"
-	"manager_project/config/migrations"
+	"manager_project/db"
+	"manager_project/db/migrations"
 )
 
 func main() {
-	db, err := config.ConnectDb()
+	database, err := db.ConnectDb()
 	if err != nil {
 		log.Fatalf("Could not connect to the database: %v", err)
 	}
 
 	log.Println("Starting migrations...")
-	if err := migrations.RunMigrations(db); err != nil {
+	if err := migrations.RunMigrations(database); err != nil {
 		log.Fatalf("Could not run migrations: %v", err)
 	}
 	log.Println("Migrations completed successfully")
