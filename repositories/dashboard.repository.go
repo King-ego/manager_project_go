284f9752fe5a5c4811a3e2910b2726f08874ec3e
Diego <diegosantos4957@gmail.com>
2026-06-29 18:30:21 -0300

    feat: implement dashboard repository

--- repositories/dashboard.repository.go
+++ repositories/dashboard.repository.go
@@ -1,1 +1,25 @@
 package repositories
+
+import "gorm.io/gorm"
+
+type DashboardRepository interface {
+}
+
+type dashboardRepository struct {
+	db *gorm.DB
+}
+
+func NewDashboardRepository(db *gorm.DB) DashboardRepository {
+	return &dashboardRepository{db: db}
+}
+
+func (dr *dashboardRepository) GetDashboard() (any, error) {
+	var result any
+	err := dr.db.Raw("SELECT * FROM dashboard_data").Scan(&result).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
